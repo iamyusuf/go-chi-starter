1c79012a846722a6ad04cd52e68ae27edeab88ae
Md Yousuf Ali <yousuf3usb@gmail.com>
2024-03-18 23:59:02 +0600

    feat: add a helper method, jsonResponse

--- helpers.go
+++ helpers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func textResponse(w http.ResponseWriter, message string, status int) {
 	w.WriteHeader(status)
@@ -9,4 +12,16 @@ func textResponse(w http.ResponseWriter, message string, status int) {
 	if err != nil {
 		return
 	}
+}
+
+func jsonResponse(w http.ResponseWriter, statusCode int, data interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
+		return err
+	}
+
+	return nil
 }
