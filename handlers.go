971c961319497f2bf465203d5330aad03ef01e64
Md Yousuf Ali <yousuf3usb@gmail.com>
2024-03-19 00:00:07 +0600

    feat: move handlers to its separate file

---
+++ handlers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+)
+
+type Handler func(w http.ResponseWriter, r *http.Request) error
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := h(w, r); err != nil {
+		// handle returned error here.
+		w.WriteHeader(503)
+		_, err := w.Write([]byte("bad"))
+		if err != nil {
+			return
+		}
+	}
+}
+
+func customHandler(w http.ResponseWriter, r *http.Request) error {
+	q := r.URL.Query().Get("err")
+
+	if q != "" {
+		return errors.New(q)
+	}
+
+	_, err := w.Write([]byte("foo"))
+	return err
+}
